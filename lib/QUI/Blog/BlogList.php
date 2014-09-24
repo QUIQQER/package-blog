4a285bd759bec50f69b7de7322d87434309802d0
Henning Leutz <leutz@pcsg.de>
2014-09-24 09:17:30 +0200

    seitentype an neue controls angepasst

--- lib/QUI/Blog/BlogList.php
+++ lib/QUI/Blog/BlogList.php
@@ -1,8 +1,37 @@
 <?php
 
+/**
+ * This file contains \QUI\Blog\BlogList
+ */
+
 namespace QUI\Blog;
+
+/**
+ * Blog list helper class
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
 
 class BlogList
 {
+    /**
+     * event on child create
+     *
+     * @param Integer $newId
+     * @param \QUI\Projects\Site\Edit $Parent
+     */
+    static function onChildCreate($newId, $Parent)
+    {
+        if ( $Parent->getAttribute( 'type' ) !== 'quiqqer/blog:blog/list' ) {
+            return;
+        }
 
+        $Project = $Parent->getProject();
+        $Site    = new \QUI\Projects\Site\Edit( $Project, $newId );
+
+        $Site->setAttribute( 'nav_hide', 1 );
+        $Site->setAttribute( 'release_from', date('Y-m-d H:i:s') );
+        $Site->setAttribute( 'type', 'quiqqer/blog:blog/entry' );
+        $Site->save();
+    }
 }
