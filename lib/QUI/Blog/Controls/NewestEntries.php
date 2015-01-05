a2d478619626a763d8a55f1063707a727ff8f865
Henning Leutz <leutz@pcsg.de>
2015-01-05 21:00:52 +0100

    blog controls -> newest list + Blocks

---
+++ lib/QUI/Blog/Controls/NewestEntries.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * This file contains QUI\Blog\Controls\NewestEntries
+ */
+namespace QUI\Blog\Controls;
+
+use QUI;
+
+/**
+ * Class NewestEntries
+ *
+ * @package quiqqer/blog
+ */
+class NewestEntries extends QUI\Control
+{
+    /**
+     * constructor
+     * @param Array $attributes
+     */
+    public function __construct($attributes = array())
+    {
+        // default options
+        $this->setAttributes(array(
+            'max'     => 5,
+            'Project' => false,
+            'class'   => 'quiqqer-blog-newestentries'
+        ));
+
+        parent::setAttributes( $attributes );
+
+        $this->addCSSFile(
+            dirname(__FILE__) . '/NewestEntries.css'
+        );
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine  = QUI::getTemplateManager()->getEngine();
+        $Project = $this->_getProject();
+
+        $children = $Project->getSites(array(
+            'where' => array(
+                'type' => 'quiqqer/blog:blog/entry'
+            ),
+            'limit' => (int)$this->getAttribute( 'max' ),
+            'order' => 'release_from DESC'
+        ));
+
+
+        $Engine->assign(array(
+            'Rewrite'  => QUI::getRewrite(),
+            'children' => $children
+        ));
+
+        return $Engine->fetch( dirname( __FILE__ ) .'/NewestEntries.html' );
+    }
+
+    /**
+     * Return the Project
+     * @return QUI\Projects\Project
+     */
+    protected function _getProject()
+    {
+        if ( $this->getAttribute('Project') ) {
+            return $this->getAttribute('Project');
+        }
+
+        return QUI::getProjectManager()->get();
+    }
+
+}
