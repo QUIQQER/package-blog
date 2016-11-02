bff7975e260bce613116be2b5b81612633d0bfe3
Michael <michael.danielczok@gmail.com>
2016-11-02 13:31:05 +0100

    refactor: codestyle

--- blog/list.php
+++ blog/list.php
@@ -5,16 +5,15 @@
  */
 
 $ChildrenList = new QUI\Controls\ChildrenList(array(
-    'showContent' => false,
-    'showCreator' => $Site->getAttribute('quiqqer.settings.blog.showCreator'),
-    'showTime' => $Site->getAttribute('quiqqer.settings.blog.showTime'),
-
-    'Site' => $Site,
-    'where' => array(
+    'showContent'    => false,
+    'showCreator'    => $Site->getAttribute('quiqqer.settings.blog.showCreator'),
+    'showTime'       => $Site->getAttribute('quiqqer.settings.blog.showTime'),
+    'Site'           => $Site,
+    'where'          => array(
         'type' => 'quiqqer/blog:blog/entry'
     ),
-    'limit' => $Site->getAttribute('quiqqer.settings.blog.max'),
-    'itemtype' => "http://schema.org/ItemList",
+    'limit'          => $Site->getAttribute('quiqqer.settings.blog.max'),
+    'itemtype'       => "http://schema.org/ItemList",
     'child-itemtype' => "http://schema.org/BlogPosting",
     'display'        => $Site->getAttribute('quiqqer.settings.blog.template')
 ));
