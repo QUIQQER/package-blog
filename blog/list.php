a0376781d9923a59b9b632168317e21a1ffb06dd
Michael <michael.danielczok@gmail.com>
2017-01-04 10:24:31 +0100

    feat: Childrenlist --> mehrere Layouts integriert fix: showDate und showTime als Attribut fix: Übersetzung für Layouts kommt von Seitentypen

--- blog/list.php
+++ blog/list.php
@@ -7,7 +7,8 @@
 $ChildrenList = new QUI\Controls\ChildrenList(array(
     'showContent'    => false,
     'showCreator'    => $Site->getAttribute('quiqqer.settings.blog.showCreator'),
-    'showTime'       => $Site->getAttribute('quiqqer.settings.blog.showTime'),
+    'showTime'       => $Site->getAttribute('quiqqer.settings.news.showTime'), // sich endgültig entscheiden:
+    'showDate'       => $Site->getAttribute('quiqqer.settings.news.showTime'), // showDate vs showTime
     'Site'           => $Site,
     'where'          => array(
         'type' => 'quiqqer/blog:blog/entry'
