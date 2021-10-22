c95d80fe6187db8fb1e372e10eef55634ce1f33e
Campii <dominik.chrzanowski183@gmail.com>
2021-10-22 11:31:29 +0200

    feat: Prepare basic file structure [Author] quiqqer/package-blog#10

---
+++ lib/QUI/Blog/Controls/Author.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file contains \QUI\Bricks\Controls\TextAndImageMultiple
+ */
+
+namespace QUI\Blog\Controls;
+
+use QUI;
+
+/**
+ * Class Author
+ *
+ * @author  Dominik Chrzanowski
+ * @package quiqqer/bricks
+ */
+class Author extends QUI\Control
+{
+    /**
+     * constructor
+     *
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        // default options
+        $this->setAttributes([
+            'avatar' => false,
+            'firstName'     => false,
+            'lastName' => false,
+        ]);
+
+        parent::__construct($attributes);
+
+        $this->addCSSFile(
+            dirname(__FILE__) . '/Author.css'
+        );
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine        = QUI::getTemplateManager()->getEngine();
+
+//        $Engine->assign([
+//            'this' => $this,
+//            'html' => $html
+//        ]);
+
+        return $Engine->fetch(dirname(__FILE__) . '/Author.html');
+    }
+}
